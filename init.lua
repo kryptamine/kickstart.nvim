93fcc681434ef9714098b93355891633a41537a3
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-05-19 01:27:58 +0300

    add lazy git

--- init.lua
+++ init.lua
@@ -52,7 +52,6 @@ require('lazy').setup {
       },
     },
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,15 +1,6 @@
 return {
   {
     'rcarriga/nvim-notify',
-    keys = {
-      {
-        '<leader>un',
-        function()
-          require('notify').dismiss { silent = true, pending = true }
-        end,
-        desc = 'Dismiss All Notifications',
-      },
-    },
     opts = {
       stages = 'static',
       timeout = 3000,
