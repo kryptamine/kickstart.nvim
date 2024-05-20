35768e9115fdeafbf70fd44ce6ec8aae2ea886a6
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-05-20 10:08:59 +0300

    configure toggle-term

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
       ensure_installed = {
         'javascript',
         'typescript',
