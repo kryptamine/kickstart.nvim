c33a8db93dd429f15def897396a1e286917bba03
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-05-23 10:33:53 +0300

    improve lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,3 @@
-local servers = {
-  'bashls',
-  'cssls',
-  'dockerls',
-  'eslint',
-  'gdscript',
-  'gleam',
-  'gopls',
-  'html',
-  'htmx',
-  'jsonls',
-  'lexical',
-  'lua_ls',
-  'mdx_analyzer',
-  'pyright',
-  'taplo',
-  'templ',
-  'tsserver',
-}
-
 return {
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -26,41 +6,12 @@ return {
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
       { 'folke/neodev.nvim', opts = {} },
+      -- Automatically install LSPs and related tools to stdpath for Neovim
+      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
     },
     config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-      local lspconfig = require 'lspconfig'
-
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup {
-          -- on_attach = on_attach,
-          -- capabilities = capabilities,
-        }
-      end
-
-      lspconfig.tsserver.setup {
-        capabilities = capabilities,
-        init_options = {
-          preferences = {
-            quotePreference = 'single',
-            includeCompletionsForModuleExports = true,
-            includeCompletionsForImportStatements = true,
-            includeCompletionsWithSnippetText = true,
-            includeAutomaticOptionalChainCompletions = true,
-            includeCompletionsWithClassMemberSnippets = true,
-            includeCompletionsWithInsertText = true,
-            importModuleSpecifierPreference = 'non-relative',
-            importModuleSpecifierEnding = 'minimal',
-            provideRefactorNotApplicableReason = true,
-            allowRenameOfImportPath = true,
-            allowTextChangesInNewFiles = true,
-            displayPartsForJSDoc = true,
-            generateReturnInDocTemplate = true,
-          },
-        },
-      }
-
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -146,6 +97,91 @@ return {
           end
         end,
       })
+
+      -- LSP servers and clients are able to communicate to each other what features they support.
+      --  By default, Neovim doesn't support everything that is in the LSP specification.
+      --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
+      --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+      -- Enable the following language servers
+      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+      --
+      --  Add any additional override configuration in the following tables. Available keys are:
+      --  - cmd (table): Override the default command used to start the server
+      --  - filetypes (table): Override the default list of associated filetypes for the server
+      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
+      --  - settings (table): Override the default settings passed when initializing the server.
+      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      local servers = {
+        tsserver = {
+          {
+            capabilities = capabilities,
+            init_options = {
+              preferences = {
+                quotePreference = 'single',
+                includeCompletionsForModuleExports = true,
+                includeCompletionsForImportStatements = true,
+                includeCompletionsWithSnippetText = true,
+                includeAutomaticOptionalChainCompletions = true,
+                includeCompletionsWithClassMemberSnippets = true,
+                includeCompletionsWithInsertText = true,
+                importModuleSpecifierPreference = 'non-relative',
+                importModuleSpecifierEnding = 'minimal',
+                provideRefactorNotApplicableReason = true,
+                allowRenameOfImportPath = true,
+                allowTextChangesInNewFiles = true,
+                displayPartsForJSDoc = true,
+                generateReturnInDocTemplate = true,
+              },
+            },
+          },
+        },
+        lua_ls = {
+          -- cmd = {...},
+          -- filetypes = { ...},
+          -- capabilities = {},
+          settings = {
+            Lua = {
+              completion = {
+                callSnippet = 'Replace',
+              },
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              -- diagnostics = { disable = { 'missing-fields' } },
+            },
+          },
+        },
+      }
+
+      -- Ensure the servers and tools above are installed
+      --  To check the current status of installed tools and/or manually install
+      --  other tools, you can run
+      --    :Mason
+      --
+      --  You can press `g?` for help in this menu.
+      require('mason').setup()
+
+      -- You can add other tools here that you want Mason to install
+      -- for you, so that they are available from within Neovim.
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        'stylua', -- Used to format Lua code
+      })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      require('mason-lspconfig').setup {
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
+          end,
+        },
+      }
     end,
   },
 }
