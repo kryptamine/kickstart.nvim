595d9eeeae6e858a420717eaf90235a70cb8cb65
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-06-19 21:23:12 +0300

    WIP

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -5,34 +5,19 @@ return {
     opts = {
       signs = {
         add = {
-          hl = 'GitSignsAdd',
           text = icons.ui.BoldLineLeft,
-          numhl = 'GitSignsAddNr',
-          linehl = 'GitSignsAddLn',
         },
         change = {
-          hl = 'GitSignsChange',
           text = icons.ui.BoldLineLeft,
-          numhl = 'GitSignsChangeNr',
-          linehl = 'GitSignsChangeLn',
         },
         delete = {
-          hl = 'GitSignsDelete',
           text = icons.ui.TriangleShortArrowRight,
-          numhl = 'GitSignsDeleteNr',
-          linehl = 'GitSignsDeleteLn',
         },
         topdelete = {
-          hl = 'GitSignsDelete',
           text = icons.ui.TriangleShortArrowRight,
-          numhl = 'GitSignsDeleteNr',
-          linehl = 'GitSignsDeleteLn',
         },
         changedelete = {
-          hl = 'GitSignsChange',
           text = icons.ui.BoldLineLeft,
-          numhl = 'GitSignsChangeNr',
-          linehl = 'GitSignsChangeLn',
         },
       },
       signcolumn = true,
@@ -63,7 +48,6 @@ return {
         row = 0,
         col = 1,
       },
-      yadm = { enable = false },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
 
@@ -71,15 +55,15 @@ return {
           vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
         end
 
-      -- stylua: ignore start
-      map("n", "]h", function() gs.nav_hunk("next") end, "Next Hunk")
-      map("n", "[h", function() gs.nav_hunk("prev") end, "Prev Hunk")
-      map('n', '<leader>gd', gs.diffthis, 'Diff This')
-      map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
-      map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk Inline")
-      map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
-      map("n", "<leader>gq", ":only<CR>", "Quit Diff")
+        -- stylua: ignore start
+        map("n", "]h", function() gs.nav_hunk("next") end, "Next Hunk")
+        map("n", "[h", function() gs.nav_hunk("prev") end, "Prev Hunk")
+        map('n', '<leader>gd', gs.diffthis, 'Diff This')
+        map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk Inline")
+        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map("n", "<leader>gq", ":only<CR>", "Quit Diff")
       end,
     },
   },
