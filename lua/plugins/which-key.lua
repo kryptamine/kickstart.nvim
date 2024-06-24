ae566e4562306b83400c23958d630fa112c8d1cd
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-06-24 16:27:40 +0300

    wip

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -9,7 +9,7 @@ return {
         group = '+', -- symbol prepended to a group
       },
       window = {
-        border = 'none', -- none, single, double, shadow
+        border = 'single', -- none, single, double, shadow
         position = 'bottom', -- bottom, top
         margin = { 2, 0, 2, 0 }, -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
