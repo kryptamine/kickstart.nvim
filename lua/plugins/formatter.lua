319f7622ecf992bd57a871f73f41e29ffb92d87b
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-06-26 16:07:46 +0300

    add python formatters

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -6,17 +6,19 @@ local prettier_langs = {
   'css',
   'html',
   'json',
+  'yaml',
+  'markdown',
 }
 
 local options = {
-  -- log_level = vim.log.levels.DEBUG,
-  format_after_save = {
-    lsp_fallback = true,
-    async = true,
+  format_on_save = {
+    lsp_format = 'fallback',
+    timeout_ms = 5000,
   },
 
   formatters_by_ft = {
     lua = { 'stylua' },
+    python = { 'isort', 'black' },
   },
 
   formatters = {},
