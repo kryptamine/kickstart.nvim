4ba5fcdc07611db05683f81dd43530fb2596be73
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-06-26 12:18:12 +0300

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,7 +15,6 @@ return {
       'nvim-telescope/telescope-ui-select.nvim',
       -- Useful for getting pretty icons, but requires a Nerd Font.
       'nvim-tree/nvim-web-devicons',
-      'fdschmidt93/telescope-egrepify.nvim',
     },
     config = function()
       local telescope = require 'telescope'
@@ -32,33 +31,12 @@ return {
         end,
       })
 
-      -- Function to display only the last 5 path segments
-      local function filenameFirst(_, path)
-        local segments = {}
-        for segment in string.gmatch(path, '[^/]+') do
-          table.insert(segments, segment)
-        end
-
-        local tail = segments[#segments]
-        local parent_segments = {}
-        for i = math.max(1, #segments - 5), #segments - 1 do
-          table.insert(parent_segments, segments[i])
-        end
-
-        local parent = table.concat(parent_segments, '/')
-        if parent == '' then
-          return tail
-        end
-        return string.format('%s\t\t%s', tail, parent)
-      end
-
       telescope.setup {
         pickers = {
           oldfiles = {
             cwd_only = true,
           },
           find_files = {
-            path_display = filenameFirst,
             previewer = false,
             layout_config = {
               width = 0.4,
@@ -66,7 +44,6 @@ return {
             },
           },
           buffers = {
-            path_display = filenameFirst,
             mappings = {
               i = {
                 ['<c-d>'] = actions.delete_buffer,
@@ -84,18 +61,13 @@ return {
             },
           },
           git_status = {
-            path_display = filenameFirst,
             initial_mode = 'normal',
             layout_config = {
               preview_width = 0.6,
             },
           },
-          live_grep = {
-            path_display = filenameFirst,
-          },
           lsp_references = {
             show_line = false,
-            path_display = filenameFirst,
             initial_mode = 'normal',
           },
           treesitter = {
@@ -115,6 +87,11 @@ return {
           layout_config = {
             prompt_position = 'top',
             preview_cutoff = 120,
+          },
+          path_display = {
+            filename_first = {
+              reverse_directories = false,
+            },
           },
           vimgrep_arguments = {
             'rg',
@@ -168,11 +145,9 @@ return {
       -- Enable Telescope extensions if they are installed
       telescope.load_extension 'fzf'
       telescope.load_extension 'ui-select'
-      telescope.load_extension 'egrepify'
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
-      local extensions = require('telescope').extensions
       local utils = require 'telescope.utils'
 
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -180,7 +155,7 @@ return {
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
       vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, { desc = '[S]earch [S]ymbols' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', extensions.egrepify.egrepify, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', function()
         builtin.find_files { cwd = utils.buffer_dir() }
       end, { desc = '[S]earch in [D]irectory' })
