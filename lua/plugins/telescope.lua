a055e60c3fe811af89d9f96b35dc590017ee5013
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-06-30 21:52:48 +0300

    wip

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -142,6 +142,7 @@ return {
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
 
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
@@ -150,7 +151,7 @@ return {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = '[ ] Find files' })
+      vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Find files' })
       vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits' })
       vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[G]it [S]tatus' })
 
