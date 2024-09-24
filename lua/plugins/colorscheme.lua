8635d8f73921a36392622c41c604b7db91d53b64
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-09-24 17:21:13 +0300

    wip

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,52 +1,5 @@
 return {
   {
-    'Mofiqul/vscode.nvim',
-    name = 'vscode',
-    priority = 1000,
-    config = function()
-      local colors = require('vscode.colors').get_colors()
-      require('vscode').setup {
-        -- Enable transparent background
-        transparent = false,
-
-        -- Enable italic comment
-        italic_comments = true,
-
-        -- Underline `@markup.link.*` variants
-        underline_links = false,
-        group_overrides = {
-          TelescopePreviewBorder = { bg = colors.vscLeftDark, fg = colors.vscLeftDark },
-          TelescopePreviewNormal = { bg = colors.vscLeftDark },
-          TelescopePreviewTitle = { fg = colors.vscLeftDark, bg = colors.vscLeftDark },
-          TelescopePromptBorder = { bg = colors.vscContext, fg = colors.vscContext },
-          TelescopePromptCounter = { fg = '#FFFFFF' },
-          TelescopePromptNormal = { bg = colors.vscContext },
-          TelescopePromptPrefix = { bg = colors.vscContext, fg = colors.vscRed },
-          TelescopePromptTitle = { fg = '#FFFFFF', bg = colors.vscRed },
-          TelescopeResultsBorder = { bg = colors.vscLeftDark, fg = colors.vscLeftDark },
-          TelescopeResultsNormal = { bg = colors.vscLeftDark },
-          TelescopeResultsTitle = { fg = colors.vscLeftDark, bg = colors.vscLeftDark },
-        },
-      }
-
-      -- vim.api.nvim_command 'colorscheme vscode'
-    end,
-  },
-  {
-    'fcancelinha/nordern.nvim',
-    branch = 'master',
-    priority = 1000,
-    config = function()
-      require('nordern').setup {
-        transparent = false,
-        brighter_comments = true,
-        italic_comments = true,
-        italicized_comments = true,
-      }
-      -- vim.api.nvim_command 'colorscheme nordern'
-    end,
-  },
-  {
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
