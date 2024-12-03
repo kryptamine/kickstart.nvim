e7bad26a97332c9a5b1a035d19511737f192ab70
Alexander Satretdinov <alexander.satretdinov@webpros.com>
2024-12-04 00:24:09 +0200

    wip

---
+++ lua/plugins/tiny-inline-diagnostics.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'rachartier/tiny-inline-diagnostic.nvim',
+    event = 'VeryLazy', -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+      vim.diagnostic.config { virtual_text = false }
+
+      require('tiny-inline-diagnostic').setup {
+        hi = {
+          mixing_color = '#RRGGBB',
+        },
+        preset = 'classic',
+      }
+    end,
+  },
+}
